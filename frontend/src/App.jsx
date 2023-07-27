04dba1f30f065426fd8159b33fe2a233f43f2294
Mozahedul <mozahed001@gmail.com>
2023-07-27 23:45:43 +0600

    Routing setup with react-router-dom NPM package

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
 // import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h2 className='w-full h-screen text-4xl flex items-center justify-center'>
-        A MERN project with vite bundler
-      </h2>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className='w-full h-screen text-4xl flex items-center justify-center'>
+        A MERN project with vite bundler from Home
+      </h2>
+    </div>
+  );
+};
+
+export default Home;
