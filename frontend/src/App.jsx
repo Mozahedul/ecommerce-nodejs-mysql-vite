8ff8588282833f824df9593cfefa7ced978e84f3
Mozahedul <mozahed001@gmail.com>
2023-07-27 23:38:45 +0600

    Initial project setup with tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2 className='w-full h-screen text-4xl flex items-center justify-center'>
+        A MERN project with vite bundler
+      </h2>
+    </>
+  );
+}
+
+export default App;
